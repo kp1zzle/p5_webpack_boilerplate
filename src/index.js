e6a4967048b7ed63483e0da087183a58c5f1f192
Kieran Sagar Parikh <kieran@parikh.ca>
2023-03-14 22:36:14 -0400

    p5 boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello world2")
+import * as p5 from 'p5'
+
+let sketch = (s) => {
+    s.setup = () => {
+        s.createCanvas(window.innerWidth, window.innerHeight)
+        s.background(220);
+    }
+
+    s.draw = () => {
+        s.textSize(50);
+        s.text('hello world', 10, 50);
+    }
+}
+
+const P5 = new p5(sketch);
